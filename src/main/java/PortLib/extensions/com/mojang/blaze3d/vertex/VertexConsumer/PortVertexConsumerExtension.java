d9718d021724857332f4e943442671bdbed66d7c
cooobird <2553199163@qq.com>
2026-04-17 18:56:37 +0800

    refactor: 为 VertexConsumer 添加扩展方法
    - 新增 PortVertexConsumerExtension 扩展方法类
    - 实现 addVertex、setColor、setUv、setNormal 等方法
    - 提供对 Vector3f 和 Matrix4f 的顶点添加支持
    - 保持原有功能不变，仅改变调用方式为扩展方法

---
+++ src/main/java/PortLib/extensions/com/mojang/blaze3d/vertex/VertexConsumer/PortVertexConsumerExtension.java
@@ -0,0 +1,62 @@
+package PortLib.extensions.com.mojang.blaze3d.vertex.VertexConsumer;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+import net.minecraft.util.FastColor;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+@Extension
+public class PortVertexConsumerExtension {
+
+    public static VertexConsumer addVertex(@This VertexConsumer thiz, float x, float y, float z) {
+        return thiz.vertex(x, y, z);
+    }
+
+    public static VertexConsumer setColor(@This VertexConsumer thiz, int r, int g, int b, int a) {
+        return thiz.color(r, g, b, a);
+    }
+
+    public static VertexConsumer setColor(@This VertexConsumer thiz, int color) {
+        return thiz.color(color);
+    }
+
+    public static VertexConsumer setUv(@This VertexConsumer thiz, float u, float v) {
+        return thiz.uv(u, v);
+    }
+
+    public static VertexConsumer setUv1(@This VertexConsumer thiz, int u, int v) {
+        return thiz.overlayCoords(u, v);
+    }
+
+    public static VertexConsumer setUv2(@This VertexConsumer thiz, int u, int v) {
+        return thiz.uv2(u, v);
+    }
+
+    public static VertexConsumer setNormal(@This VertexConsumer thiz, float x, float y, float z) {
+        return thiz.normal(x, y, z);
+    }
+
+    public static VertexConsumer setLight(@This VertexConsumer thiz, int packedLight) {
+        return thiz.uv2(packedLight);
+    }
+
+    public static VertexConsumer setOverlay(@This VertexConsumer thiz, int packedOverlay) {
+        return thiz.overlayCoords(packedOverlay);
+    }
+
+    public static VertexConsumer setWhiteAlpha(@This VertexConsumer thiz, int alpha) {
+        return thiz.color(FastColor.ARGB32.color(alpha, 255, 255, 255));
+    }
+
+    public static VertexConsumer addVertex(@This VertexConsumer thiz, Vector3f pos) {
+        return thiz.addVertex(pos.x(), pos.y(), pos.z());
+    }
+
+    public static VertexConsumer addVertex(@This VertexConsumer thiz, Matrix4f pose, float x, float y, float z) {
+        Vector4f vector4f = pose.transform(new Vector4f(x, y, z, 1.0F));
+        return thiz.addVertex(vector4f.x(), vector4f.y(), vector4f.z());
+    }
+}
