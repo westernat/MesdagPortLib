dafa09d33aae2168fc074afa382f5122bdec0e89
cooobird <2553199163@qq.com>
2026-04-18 10:20:36 +0800

    feat: 为 List 扩展添加首尾元素操作方法
    - 新增 addFirst 和 addLast 方法，支持在列表首尾添加元素
    - 针对 LinkedList 进行优化处理，直接调用其原生方法
    - 保持原有 getFirst 和 getLast 方法不变，扩展了列表操作方法

--- src/main/java/PortLib/extensions/java/util/List/PortListExtension.java
+++ src/main/java/PortLib/extensions/java/util/List/PortListExtension.java
@@ -3,12 +3,16 @@ package PortLib.extensions.java.util.List;
 import manifold.ext.rt.api.Extension;
 import manifold.ext.rt.api.This;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Extension
 public class PortListExtension {
     public static <E> E getFirst(@This List<E> thiz) {
+        if (thiz instanceof LinkedList<E> linkedList) {
+            return linkedList.getFirst();
+        }
         if (thiz.isEmpty()) {
             throw new NoSuchElementException();
         }
@@ -16,9 +20,28 @@ public class PortListExtension {
     }
 
     public static <E> E getLast(@This List<E> thiz) {
+        if (thiz instanceof LinkedList<E> linkedList) {
+            return linkedList.getLast();
+        }
         if (thiz.isEmpty()) {
             throw new NoSuchElementException();
         }
         return thiz.get(thiz.size() - 1);
+    }
+
+    public static <E> void addFirst(@This List<E> thiz, E e) {
+        if (thiz instanceof LinkedList<E> linkedList) {
+            linkedList.addFirst(e);
+        } else {
+            thiz.add(0, e);
+        }
+    }
+
+    public static <E> void addLast(@This List<E> thiz, E e) {
+        if (thiz instanceof LinkedList<E> linkedList) {
+            linkedList.addLast(e);
+        } else {
+            thiz.add(e);
+        }
     }
 }
